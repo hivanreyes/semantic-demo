cedcd811db9390c7262bc80ae1cd4fa61ec7483a
Ivan Reyes <ivan.reyes@wizeline.com>
2018-04-29 20:04:09 -0500

    Add featured section

--- client/app/home/featured.jsx
+++ client/app/home/featured.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../../components';
 import style from '../../styles/components/_featured.scss';
 
 const Featured = () => (
@@ -8,6 +9,20 @@ const Featured = () => (
       className={style.image}
       alt="Exploration for everyone"
     />
+    <div className={style.descriptionContainer}>
+      <div className={style.position}>
+        GIRONA, SPAIN, APR 15 2018
+      </div>
+      <div className={style.title}>
+        Hidden deserts - discovering barrens worldwide
+      </div>
+      <div className={style.description}>
+        Underwater macroalgal forests are among the most important ecosystems in our oceans. Their fronds and branches create a rich canopy, which harbors a diversity of species that are critical to nearshore trophic networks. Additionally, macroalgal forests offer a wide range of goods and services to populations living on the coast. They help ensure high water quality, provide refuge to species of
+      </div>
+      <div className={style.buttons}>
+        <Button label="Follow" />
+      </div>
+    </div>
   </div>
 );
 
